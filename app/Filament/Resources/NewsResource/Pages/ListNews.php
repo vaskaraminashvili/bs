fef5586f207e0f74a19c994ff4ca8bccd37aac40
Vasili Raminashvili <vaska.r02@gmail.com>
2024-12-14 16:37:26 +0400

    installed debagbur

--- app/Filament/Resources/NewsResource/Pages/ListNews.php
+++ app/Filament/Resources/NewsResource/Pages/ListNews.php
@@ -5,6 +5,9 @@ namespace App\Filament\Resources\NewsResource\Pages;
 use App\Filament\Resources\NewsResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
+use Illuminate\Contracts\Pagination\Paginator;
+use Illuminate\Database\Eloquent\Builder;
+
 
 class ListNews extends ListRecords
 {
@@ -17,5 +20,10 @@ class ListNews extends ListRecords
             Actions\CreateAction::make(),
             Actions\LocaleSwitcher::make(),
         ];
+    }
+
+    protected function paginateTableQuery(Builder $query): Paginator
+    {
+        return $query->simplePaginate(($this->getTableRecordsPerPage() === 'all') ? $query->count() : $this->getTableRecordsPerPage());
     }
 }
